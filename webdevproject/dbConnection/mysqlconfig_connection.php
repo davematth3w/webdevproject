9abce8864cf4288a0cf7addadbac2fad4860662b
davematth3w <143577093+davematth3w@users.noreply.github.com>
2023-10-31 13:07:10 +0800

    webdevproject

---
+++ webdevproject/dbConnection/mysqlconfig_connection.php
@@ -0,0 +1,12 @@
+
+
+<?php
+    DEFINE('DB_USER', 'root');
+    DEFINE('DB_PASSWORD', 'root');
+    DEFINE('DB_HOST', 'localhost');
+    DEFINE('DB_NAME', 'db');
+
+    $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+    OR die('Could not connect to MySQL: ' . mysqli_connect_error());
+    // OR dies('Could not connect to MySQL: ' . mysqli_connect_error());
+?>
